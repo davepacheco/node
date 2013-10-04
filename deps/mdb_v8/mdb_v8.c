c10cc3e0298bfd56bf37d1b101a13fb23382abe6
Dave Pacheco <dap@joyent.com>
2013-10-04 09:38:58 -0700

    add support for sliced strings to mdb_v8

--- deps/mdb_v8/mdb_v8.c
+++ deps/mdb_v8/mdb_v8.c
@@ -127,6 +127,7 @@ static intptr_t	V8_AsciiStringTag;
 static intptr_t	V8_StringRepresentationMask;
 static intptr_t	V8_SeqStringTag;
 static intptr_t	V8_ConsStringTag;
+static intptr_t	V8_SlicedStringTag;
 static intptr_t	V8_ExternalStringTag;
 static intptr_t	V8_FailureTag;
 static intptr_t	V8_FailureTagMask;
@@ -195,6 +196,8 @@ static ssize_t V8_OFF_SHAREDFUNCTIONINFO_INFERRED_NAME;
 static ssize_t V8_OFF_SHAREDFUNCTIONINFO_LENGTH;
 static ssize_t V8_OFF_SHAREDFUNCTIONINFO_SCRIPT;
 static ssize_t V8_OFF_SHAREDFUNCTIONINFO_NAME;
+static ssize_t V8_OFF_SLICEDSTRING_PARENT;
+static ssize_t V8_OFF_SLICEDSTRING_OFFSET;
 static ssize_t V8_OFF_STRING_LENGTH;
 
 #define	NODE_OFF_EXTSTR_DATA		0x4	/* see node_string.h */
@@ -242,6 +245,8 @@ static v8_constant_t v8_constants[] = {
 	{ &V8_StringRepresentationMask,	"v8dbg_StringRepresentationMask" },
 	{ &V8_SeqStringTag,		"v8dbg_SeqStringTag"		},
 	{ &V8_ConsStringTag,		"v8dbg_ConsStringTag"		},
+	{ &V8_SlicedStringTag,		"v8dbg_SlicedStringTag",
+	    V8_CONSTANT_FALLBACK(0, 0), 0x3 },
 	{ &V8_ExternalStringTag,	"v8dbg_ExternalStringTag"	},
 	{ &V8_FailureTag,		"v8dbg_FailureTag"		},
 	{ &V8_FailureTagMask,		"v8dbg_FailureTagMask"		},
@@ -293,6 +298,7 @@ typedef struct v8_offset {
 	const char	*v8o_class;
 	const char	*v8o_member;
 	boolean_t	v8o_optional;
+	intptr_t	v8o_fallback;
 } v8_offset_t;
 
 static v8_offset_t v8_offsets[] = {
@@ -362,6 +368,10 @@ static v8_offset_t v8_offsets[] = {
 	    "SharedFunctionInfo", "name" },
 	{ &V8_OFF_SHAREDFUNCTIONINFO_SCRIPT,
 	    "SharedFunctionInfo", "script" },
+	{ &V8_OFF_SLICEDSTRING_OFFSET,
+	    "SlicedString", "offset" },
+	{ &V8_OFF_SLICEDSTRING_PARENT,
+	    "SlicedString", "parent", B_TRUE },
 	{ &V8_OFF_STRING_LENGTH,
 	    "String", "length" },
 };
@@ -529,6 +539,10 @@ again:
 
 	if (V8_OFF_SEQTWOBYTESTR_CHARS == -1)
 		V8_OFF_SEQTWOBYTESTR_CHARS = V8_OFF_SEQASCIISTR_CHARS;
+
+	if (V8_OFF_SLICEDSTRING_PARENT == -1)
+		V8_OFF_SLICEDSTRING_PARENT = V8_OFF_SLICEDSTRING_OFFSET -
+		    sizeof (uintptr_t);
 
 	return (failed ? -1 : 0);
 }
@@ -1310,7 +1324,8 @@ obj_print_class(uintptr_t addr, v8_class_t *clp)
  * Print the ASCII string for the given JS string, expanding ConsStrings and
  * ExternalStrings as needed.
  */
-static int jsstr_print_seq(uintptr_t, uint_t, char **, size_t *);
+static int jsstr_print_seq(uintptr_t, uint_t, char **, size_t *, size_t,
+    ssize_t);
 static int jsstr_print_cons(uintptr_t, uint_t, char **, size_t *);
 static int jsstr_print_sliced(uintptr_t, uint_t, char **, size_t *);
 static int jsstr_print_external(uintptr_t, uint_t, char **, size_t *);
@@ -1347,11 +1362,13 @@ jsstr_print(uintptr_t addr, uint_t flags, char **bufp, size_t *lenp)
 		flags &= ~JSSTR_ISASCII;
 
 	if (V8_STRREP_SEQ(typebyte))
-		err = jsstr_print_seq(addr, flags, bufp, lenp);
+		err = jsstr_print_seq(addr, flags, bufp, lenp, 0, -1);
 	else if (V8_STRREP_CONS(typebyte))
 		err = jsstr_print_cons(addr, flags, bufp, lenp);
 	else if (V8_STRREP_EXT(typebyte))
 		err = jsstr_print_external(addr, flags, bufp, lenp);
+	else if (V8_STRREP_SLICED(typebyte))
+		err = jsstr_print_sliced(addr, flags, bufp, lenp);
 	else {
 		(void) bsnprintf(bufp, lenp, "<unknown string type>");
 		err = -1;
@@ -1364,14 +1381,15 @@ jsstr_print(uintptr_t addr, uint_t flags, char **bufp, size_t *lenp)
 }
 
 static int
-jsstr_print_seq(uintptr_t addr, uint_t flags, char **bufp, size_t *lenp)
+jsstr_print_seq(uintptr_t addr, uint_t flags, char **bufp, size_t *lenp,
+    size_t sliceoffset, ssize_t slicelen)
 {
 	/*
 	 * To allow the caller to allocate a very large buffer for strings,
 	 * we'll allocate a buffer sized based on our input, making it at
 	 * least enough space for our ellipsis and at most 256K.
 	 */
-	uintptr_t i, nstrchrs, nreadbytes, blen, nstrbytes;
+	uintptr_t i, nstrchrs, nreadbytes, nreadoffset, blen, nstrbytes;
 	boolean_t verbose = flags & JSSTR_VERBOSE ? B_TRUE : B_FALSE;
 	boolean_t quoted = flags & JSSTR_QUOTED ? B_TRUE : B_FALSE;
 	char *buf;
@@ -1383,13 +1401,26 @@ jsstr_print_seq(uintptr_t addr, uint_t flags, char **bufp, size_t *lenp)
 	if (read_heap_smi(&nstrchrs, addr, V8_OFF_STRING_LENGTH) != 0)
 		return (-1);
 
-	nstrbytes = (flags & JSSTR_ISASCII) != 0 ? nstrchrs : 2 * nstrchrs;
+	if (slicelen != -1)
+		nstrchrs = slicelen;
+	if (nstrchrs < 0)
+		nstrchrs = 0;
+
+	if ((flags & JSSTR_ISASCII) != 0) {
+		nstrbytes = nstrchrs;
+		nreadoffset = sliceoffset;
+	} else {
+		nstrbytes = 2 * nstrchrs;
+		nreadoffset = 2 * sliceoffset;
+	}
+
 	nreadbytes = nstrbytes + sizeof ("\"\"") <= blen ? nstrbytes :
 	    blen - sizeof ("\"\"[...]");
 
 	if (verbose)
-		mdb_printf("length: %d chars (%d bytes), will read: %d bytes\n",
-		    nstrchrs, nstrbytes, nreadbytes);
+		mdb_printf("length: %d chars (%d bytes), "
+		    "will read %d bytes from offset %d\n",
+		    nstrchrs, nstrbytes, nreadbytes, nreadoffset);
 
 	if (nstrbytes == 0) {
 		(void) bsnprintf(bufp, lenp, "%s%s",
@@ -1401,7 +1432,7 @@ jsstr_print_seq(uintptr_t addr, uint_t flags, char **bufp, size_t *lenp)
 
 	if ((flags & JSSTR_ISASCII) != 0) {
 		if (mdb_readstr(buf, nreadbytes + 1,
-		    addr + V8_OFF_SEQASCIISTR_CHARS) == -1) {
+		    addr + V8_OFF_SEQASCIISTR_CHARS + nreadoffset) == -1) {
 			v8_warn("failed to read SeqString data");
 			return (-1);
 		}
@@ -1413,7 +1444,7 @@ jsstr_print_seq(uintptr_t addr, uint_t flags, char **bufp, size_t *lenp)
 		    quoted ? "\"" : "", buf, quoted ? "\"" : "");
 	} else {
 		if (mdb_readstr(buf, nreadbytes,
-		    addr + V8_OFF_SEQTWOBYTESTR_CHARS) == -1) {
+		    addr + V8_OFF_SEQTWOBYTESTR_CHARS + nreadoffset) == -1) {
 			v8_warn("failed to read SeqTwoByteString data");
 			return (-1);
 		}
@@ -1461,6 +1492,52 @@ jsstr_print_cons(uintptr_t addr, uint_t flags, char **bufp, size_t *lenp)
 	if (quoted)
 		(void) bsnprintf(bufp, lenp, "\"");
 
+	return (0);
+}
+
+static int
+jsstr_print_sliced(uintptr_t addr, uint_t flags, char **bufp, size_t *lenp)
+{
+	uintptr_t parent, offset, length;
+	uint8_t typebyte;
+	boolean_t verbose = flags & JSSTR_VERBOSE ? B_TRUE : B_FALSE;
+	boolean_t quoted = flags & JSSTR_QUOTED ? B_TRUE : B_FALSE;
+	uint_t newflags;
+
+	if (read_heap_ptr(&parent, addr, V8_OFF_SLICEDSTRING_PARENT) != 0 ||
+	    read_heap_smi(&offset, addr, V8_OFF_SLICEDSTRING_OFFSET) != 0 ||
+	    read_heap_smi(&length, addr, V8_OFF_STRING_LENGTH) != 0)
+		return (-1);
+
+	if (verbose)
+		mdb_printf("parent: %p, offset = %d, length = %d\n",
+		    parent, offset, length);
+
+	if (read_typebyte(&typebyte, parent) != 0) {
+		v8_warn("SlicedString %s: failed to read parent's type", addr);
+		(void) bsnprintf(bufp, lenp, "<sliced string>");
+		return (0);
+	}
+
+	if (!V8_STRREP_SEQ(typebyte)) {
+		v8_warn("SlicedString %s: parent is not a sequential string",
+		    addr);
+		(void) bsnprintf(bufp, lenp, "<sliced string>");
+		return (0);
+	}
+
+	if (quoted)
+		(void) bsnprintf(bufp, lenp, "\"");
+
+	newflags = verbose ? JSSTR_VERBOSE : 0;
+	if (V8_STRENC_ASCII(typebyte))
+		newflags |= JSSTR_ISASCII;
+	if (jsstr_print_seq(parent, newflags, bufp, lenp, offset, length) != 0)
+		return (-1);
+
+	if (quoted)
+		(void) bsnprintf(bufp, lenp, "\"");
+	
 	return (0);
 }
 
